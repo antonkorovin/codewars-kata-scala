1f7fc0d58967de506f0369b0cc8b3eaaebfe68e1
Anton Korovin <korovin.anton@gmail.com>
2019-10-04 16:56:24 +0300

    [century-year] Code passes all tests

--- src/test/scala/com/codewars/kyu8/CenturyYear.scala
+++ src/test/scala/com/codewars/kyu8/CenturyYear.scala
@@ -1,5 +1,8 @@
 package com.codewars.kyu8
 
 object CenturyYear {
-  def centuryFromYear(year: Int): Int = ???
+  def centuryFromYear(year: Int): Int = {
+    val additional = if ((year % 100) == 0) 0 else 1
+    (year / 100) + additional
+  }
 }
--- src/test/scala/com/codewars/kyu8/CenturyYearSpec.scala
+++ src/test/scala/com/codewars/kyu8/CenturyYearSpec.scala
@@ -6,6 +6,15 @@ class CenturyYearSpec extends FlatSpec with Matchers {
   "centuryFromYear(1705)" should "return 18" in {
     CenturyYear.centuryFromYear(1705) should be(18)
   }
+  "centuryFromYear(17)" should "return 1" in {
+    CenturyYear.centuryFromYear(17) should be(1)
+  }
+  "centuryFromYear(99)" should "return 1" in {
+    CenturyYear.centuryFromYear(99) should be(1)
+  }
+  "centuryFromYear(100)" should "return 1" in {
+    CenturyYear.centuryFromYear(100) should be(1)
+  }
   "centuryFromYear(1900)" should "return 19" in {
     CenturyYear.centuryFromYear(1900) should be(19)
   }
