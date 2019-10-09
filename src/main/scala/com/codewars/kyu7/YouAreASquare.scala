99931c4a81f14233aae98dd8a02bb401993a8619
Anton Korovin <korovin.anton@gmail.com>
2019-10-09 14:35:32 +0300

    [you-are-a-square] Code passes all tests

--- src/main/scala/com/codewars/kyu7/YouAreASquare.scala
+++ src/main/scala/com/codewars/kyu7/YouAreASquare.scala
@@ -1,11 +1,18 @@
 package com.codewars.kyu7
 
 object YouAreASquare {
+
   object Sol {
 
     def isSquare(x: Int): Boolean = {
-      // your code here
-      true
+      x match {
+        case n if n < 0 => false
+
+        case p =>
+          val s = Math.sqrt(p).toInt
+          s * s == p
+      }
     }
   }
+
 }
