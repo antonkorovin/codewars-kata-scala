5d120e65540a17504450aa09323029d267922554
Anton Korovin <korovin.anton@gmail.com>
2019-10-09 17:13:06 +0300

    [predict-your-age] Code passes all tests

--- src/main/scala/com/codewars/kyu7/PredictYourAge.scala
+++ src/main/scala/com/codewars/kyu7/PredictYourAge.scala
@@ -3,7 +3,24 @@ package com.codewars.kyu7
 object PredictYourAge {
   object Predicter {
     def predictAge(age1: Int, age2: Int, age3: Int, age4: Int, age5: Int, age6: Int, age7: Int, age8: Int): Int = {
+      predictAgeBySeq(
+        age1
+        , age2
+        , age3
+        , age4
+        , age5
+        , age6
+        , age7
+        , age8
+      )
+    }
 
+    private def predictAgeBySeq(ages: Int*): Int = {
+      val sum: Int = ages.map(x => x * x).sum
+
+      Math.sqrt(
+        sum
+      ).toInt / 2
     }
   }
 }
