dcbc6b271b6eb7a09acd9e8be179a290d7e8b3c3
Anton Korovin <korovin.anton@gmail.com>
2019-10-09 14:56:33 +0300

    [forcing-an-option] Code passes all tests

--- src/main/scala/com/codewars/kyu7/ForcingAnOption.scala
+++ src/main/scala/com/codewars/kyu7/ForcingAnOption.scala
@@ -2,8 +2,19 @@ package com.codewars.kyu7
 
 object ForcingAnOption {
 
-  object Kata {
+  object Kata extends ImplicitOptionExtraction
 
+
+  trait ImplicitOptionExtraction {
+
+    import scala.languageFeature.implicitConversions
+
+    implicit def extractValue(t: Option[Int]): Int = {
+      t match {
+        case Some(value) => value
+        case None => 0
+      }
+    }
   }
 
 }
