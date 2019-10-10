a367c4a2c8aef1b21da636eb074e055d2b031e7f
Anton Korovin <korovin.anton@gmail.com>
2019-10-10 13:51:25 +0300

    [house-numbers-sum] Code passes all tests

--- src/main/scala/com/codewars/kyu7/HouseNumbersSum.scala
+++ src/main/scala/com/codewars/kyu7/HouseNumbersSum.scala
@@ -3,7 +3,7 @@ package com.codewars.kyu7
 object HouseNumbersSum {
   object Kata {
     def houseNumbersSum(inputArray: Array[Int]): Int = {
-      ???
+      inputArray.takeWhile(_ != 0).sum
     }
   }
 }
