55fe3c7b610169056ae9c6db2abebfbd14bf955b
Anton Korovin <korovin.anton@gmail.com>
2019-10-15 18:13:47 +0300

    [sort-by-last-ch] Code passes all tests

--- src/main/scala/com/codewars/kyu7/SortByLastChar.scala
+++ src/main/scala/com/codewars/kyu7/SortByLastChar.scala
@@ -2,6 +2,9 @@ package com.codewars.kyu7
 
 object SortByLastChar {
   object Kata {
-    def last(s: String): Array[String] = ???
+    def last(s: String): Array[String] = {
+      s.split(' ')
+       .sortWith(_.last < _.last)
+    }
   }
 }
