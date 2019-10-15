f9cd07bacd5c6969548ba224740957622c916f14
Anton Korovin <korovin.anton@gmail.com>
2019-10-15 16:58:52 +0300

    [alphabetical-addition] Added solution skeleton.

---
+++ src/main/scala/com/codewars/kyu7/AlphabeticalAddition.scala
@@ -0,0 +1,12 @@
+package com.codewars.kyu7
+
+object AlphabeticalAddition {
+
+  object Kata {
+    def addLetters(letters: List[Char]): Char = {
+      // your code here
+      ???
+    }
+  }
+
+}
---
+++ src/test/scala/com/codewars/kyu7/AlphabeticalAdditionTest.scala
@@ -0,0 +1,22 @@
+package com.codewars.kyu7
+
+import org.scalatest._
+
+class AlphabeticalAdditionTest  extends FlatSpec with Matchers {
+  private val Kata = AlphabeticalAddition.Kata
+  
+  List(
+    (List('a', 'b', 'c'), 'f'),
+    (List('z'), 'z'),
+    (List('a', 'b'), 'c'),
+    (List('c'), 'c'),
+    (List('z', 'a'), 'a'),
+    (List('y', 'c', 'b'), 'd'),
+    (List[Char](), 'z')
+  ).foreach {
+    case (input, expected) =>
+      s"addLetters($input)" should s"return $expected" in {
+        Kata.addLetters(input) shouldBe expected
+      }
+  }
+}
