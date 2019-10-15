ac3572a133c8447e8b8765c1f5f2db008c1a70fc
Anton Korovin <korovin.anton@gmail.com>
2019-10-15 13:57:08 +0300

    [factorial] Added solution skeleton.

---
+++ src/main/scala/com/codewars/kyu7/Factorial.scala
@@ -0,0 +1,7 @@
+package com.codewars.kyu7
+
+object Factorial {
+  object Kata {
+    def !(n: Int): BigInt = ???
+  }
+}
---
+++ src/test/scala/com/codewars/kyu7/FactorialTest.scala
@@ -0,0 +1,23 @@
+package com.codewars.kyu7
+
+import org.scalatest.FunSuite
+
+class FactorialTest  extends FunSuite {
+
+  private val Kata = Factorial.Kata
+
+  test("Samples") {
+    assert((0!) === 1)
+    assert((1!) === 1)
+    assert((2!) === 2)
+    assert((3!) === 6)
+    assert((4!) === 24)
+    assert((5!) === 120)
+    assert((6!) === 720)
+    assert((7!) === 5040)
+  }
+
+  implicit class IntExtension(n: Int) {
+    def !(): BigInt = Kata.!(n)
+  }
+}
