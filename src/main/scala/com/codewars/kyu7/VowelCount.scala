d93a249f641c94adc7fe0e1d70397024cf09de32
Anton Korovin <korovin.anton@gmail.com>
2019-10-15 15:27:19 +0300

    [vowel-count] Added solution skeleton.

---
+++ src/main/scala/com/codewars/kyu7/VowelCount.scala
@@ -0,0 +1,15 @@
+package com.codewars.kyu7
+
+object VowelCount {
+
+  // Return the number (count) of vowels in the given string.
+  // We will consider a, e, i, o, and u as vowels for this Kata.
+  // The input string will only consist of lower case letters and/or spaces.
+  object Kata {
+    def getCount(inputStr: String): Int = {
+      // your code here
+      0
+    }
+  }
+
+}
