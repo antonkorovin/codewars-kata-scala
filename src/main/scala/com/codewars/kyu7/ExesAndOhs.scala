803e6bd543d128ee4f7a9511270d675055b31d5d
Anton Korovin <korovin.anton@gmail.com>
2019-10-15 18:05:13 +0300

    [exes-ohs] Code passes all tests

--- src/main/scala/com/codewars/kyu7/ExesAndOhs.scala
+++ src/main/scala/com/codewars/kyu7/ExesAndOhs.scala
@@ -3,7 +3,7 @@ package com.codewars.kyu7
 object ExesAndOhs {
   object Kata {
     def xo(str: String): Boolean = {
-      ???
+      str.count(_.toLower == 'x') == str.count(_.toLower == 'o')
     }
   }
 }
