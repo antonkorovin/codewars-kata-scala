f2582ef3e7aba0ab5756a8179121f1ececdb51b4
Anton Korovin <korovin.anton@gmail.com>
2019-10-16 16:52:55 +0300

    [backspaces-in-string] Code passes all tests

--- src/main/scala/com/codewars/kyu6/BackspacesInString.scala
+++ src/main/scala/com/codewars/kyu6/BackspacesInString.scala
@@ -1,5 +1,7 @@
 package com.codewars.kyu6
 
+import scala.annotation.tailrec
+
 object BackspacesInString {
 
   // Assume "#" is like a backspace in string.
@@ -9,7 +11,26 @@ object BackspacesInString {
 
   object Kata {
     def cleanString(s: String): String = {
-      ???
+      @tailrec
+      def cleanRec(xs: List[Char], backspaces: Int, result: List[Char]): List[Char] = {
+        xs match {
+          case '#' :: tail =>
+            cleanRec(tail, backspaces + 1, result)
+
+          case c :: tail if backspaces == 0 =>
+            cleanRec(tail, 0, c :: result)
+
+          case _ :: tail =>
+            cleanRec(tail, backspaces - 1, result)
+
+          case Nil =>
+            result
+        }
+      }
+
+      // //////////////////////////////
+
+      cleanRec(s.toList.reverse, 0, List.empty).mkString("")
     }
   }
 
