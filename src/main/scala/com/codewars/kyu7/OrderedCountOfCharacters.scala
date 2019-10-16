8589b357ca30cbbdba28270b52b78c1afc36a44d
Anton Korovin <korovin.anton@gmail.com>
2019-10-16 14:21:32 +0300

    [ordered-count-cf-characters] Code passes all tests

--- src/main/scala/com/codewars/kyu7/OrderedCountOfCharacters.scala
+++ src/main/scala/com/codewars/kyu7/OrderedCountOfCharacters.scala
@@ -1,10 +1,39 @@
 package com.codewars.kyu7
 
+import scala.annotation.tailrec
+
 object OrderedCountOfCharacters {
+  // Count the number of occurrences of each character and return it as a list of tuples in order of appearance.
+  // Example:
+  // Kata.orderedCount("abracadabra") == List(('a', 5), ('b', 2), ('r', 2), ('c', 1), ('d', 1))
+
   object Kata {
-    def orderedCount(chars: String): List[(Char, Int)] = {
-      // Implement me!
-      List[(Char, Int)]()
+    type ResultList = List[(Char, Int)]
+
+    def orderedCount(chars: String): ResultList = {
+      @tailrec
+      def countRec(rest: List[Char], counted: ResultList): ResultList = {
+        rest match {
+          case head :: tail =>
+            val count = 1 + tail.count(_ == head)
+            val filtered = tail.filter(_ != head)
+
+            countRec(
+              filtered,
+              (head, count) :: counted
+            )
+
+          case Nil =>
+            counted
+        }
+      }
+
+      // //////////////////////////////
+
+      countRec(
+        chars.toList,
+        Nil
+      ).reverse
     }
   }
 }
